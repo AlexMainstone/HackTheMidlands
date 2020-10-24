93aac6f96fb57d6b334fa2ca40b142fe1a0c37a7
Alex Mainstone <alecsmainstone@gmail.com>
2020-10-25 00:27:15 +0100

    nn view mode

--- render.py
+++ render.py
@@ -2,26 +2,45 @@ import pyglet as pgl
 import numpy as np
 from math import sqrt
 
-np.random.seed(2)
-
+# Create Window
 window = pgl.window.Window(1280, 720, "COVID-19 SIM")
+
+# Bind key data
 key = pgl.window.key
+
+# Loac map
 map = pgl.image.load("res/map.gif")
 
+# Camera
 cam_pos = [0, 0]
 cam_input = [0, 0]
 
+# Edit Mode, allows for saving and node creation
 edit = False
+
+# Currently selected node
+closest_node = -1
 selected_node = -1
+
+# Node & Edge data
 node_list = []
 edge_list = []
 
+def clean_edges():
+    for ei in range(0, len(edge_list)-1):
+        for si in range(0, len(edge_list)-1):
+            if ei == si: continue
+            if edge_list[ei][0] == edge_list[si][0] and edge_list[ei][1] == edge_list[si][1]: del edge_list[si]
+            if edge_list[ei][0] == edge_list[si][1] and edge_list[ei][1] == edge_list[si][0]: del edge_list[si]
+
 # Load nodes and edges from file
 datafile = open("res/datafile.txt", "r")
-Lines = datafile.readlines();
+lines = datafile.readlines()
+
+# Determines if we're storing edges or points
 points = True
-for l in Lines:
-    if(l == "EDGES\n"):
+for l in lines:
+    if(l == "EDGES\n"): # This signifys that edges will follow
         points = False
         continue
 
@@ -35,7 +54,9 @@ for l in Lines:
         edge_list.append(data)
 datafile.close()
 
+# Update is called 30 times a second
 def update(dt):
+    # Update camera position
     cam_pos[0] += cam_input[0]*5
     cam_pos[1] += cam_input[1]*5
 
@@ -48,28 +69,74 @@ def on_draw():
     
     #draw edges
     nodeedge = pgl.shapes.Line(0, 0, 0, 0, 4, color=(0,0,255))
-    for edge in edge_list:
-        p1 = node_list[edge[0]]
-        p2 = node_list[edge[1]]
-        nodeedge.x = p1[0] + cam_pos[0]
-        nodeedge.y = p1[1] + cam_pos[1]
-        nodeedge.x2 = p2[0] + cam_pos[0]
-        nodeedge.y2 = p2[1] + cam_pos[1]
-        nodeedge.draw();
+    if edit:
+        for edge in edge_list:
+            # Get node data
+            p1 = node_list[edge[0]]
+            p2 = node_list[edge[1]]
+
+            # set the lines points
+            nodeedge.x = p1[0] + cam_pos[0]
+            nodeedge.y = p1[1] + cam_pos[1]
+            nodeedge.x2 = p2[0] + cam_pos[0]
+            nodeedge.y2 = p2[1] + cam_pos[1]
+            
+            nodeedge.draw()
+    else:
+        for edge in edge_list:
+            subnode = -1
+            if edge[0] == closest_node:
+                subnode = edge[1]
+            elif edge[1] == closest_node: 
+                subnode = edge[0]
+            else:
+                continue
+            
+            for e in edge_list:
+                if (e[0] != subnode and e[1] != subnode) or e[0] == closest_node or e[1] == closest_node: continue
+                nodeedge.color = (255, 100, 100)
+                # Get node data
+                p1 = node_list[e[0]]
+                p2 = node_list[e[1]]
+
+                # set the lines points
+                nodeedge.x = p1[0] + cam_pos[0]
+                nodeedge.y = p1[1] + cam_pos[1]
+                nodeedge.x2 = p2[0] + cam_pos[0]
+                nodeedge.y2 = p2[1] + cam_pos[1]
+                
+                nodeedge.draw()
+                
+            
+            nodeedge.color = (0, 0, 255)
+            # Get node data
+            p1 = node_list[edge[0]]
+            p2 = node_list[edge[1]]
+
+            # set the lines points
+            nodeedge.x = p1[0] + cam_pos[0]
+            nodeedge.y = p1[1] + cam_pos[1]
+            nodeedge.x2 = p2[0] + cam_pos[0]
+            nodeedge.y2 = p2[1] + cam_pos[1]
+            
+            nodeedge.draw()
 
     # draw nodes
     nodecircle = pgl.shapes.Circle(100, 100, 8, color=(0, 0, 0))
     for pos in node_list:
+        # Draw the outline circle
         nodecircle.radius = 8
         nodecircle.color = (0, 0, 0)
         nodecircle.x = pos[0] + cam_pos[0]
         nodecircle.y = pos[1] + cam_pos[1]
         nodecircle.draw()
         
+        # Draw inner circle
         nodecircle.radius = 5
         nodecircle.color = (255,255,255)
         nodecircle.draw()
     
+    # Draw the selection cursor
     if selected_node != -1:
         nodecircle.x = node_list[selected_node][0] + cam_pos[0]
         nodecircle.y = node_list[selected_node][1] + cam_pos[1]
@@ -80,7 +147,7 @@ def on_draw():
 
 @window.event
 def on_key_press(symbol, modifiers):
-    #key down
+    # Movement
     if symbol == key.W:
         cam_input[1] = -1
     if symbol == key.S:
@@ -90,21 +157,25 @@ def on_key_press(symbol, modifiers):
     if symbol == key.D:
         cam_input[0] = -1
     
+    # Edit mode
     if symbol == key.E:
         global edit 
-        edit = True
-    
+        edit = not edit
+
     if not edit:
         return
     
+    # Save to file
     if symbol == key.F:
+        clean_edges()
         datafile = open("res/datafile.txt", "w")
         lines = []
+        # Save nodes
         for n in node_list:
             lines.append(str(n[0]) + " " + str(n[1]) + "\n")
 
+        # Save Edges
         lines.append("EDGES\n")
-
         for e in edge_list:
             lines.append(str(e[0]) + " " + str(e[1]) + "\n")
         
@@ -112,29 +183,34 @@ def on_key_press(symbol, modifiers):
         datafile.close()
 
 @window.event
+def on_mouse_motion(x, y, dx, dy):
+    global closest_node
+    best_dist = 10000
+    for n in range(0, len(node_list)):
+        current_dist = sqrt((node_list[n][0] - (x - cam_pos[0]))**2 + (node_list[n][1] - (y - cam_pos[1]))**2)
+        if current_dist < best_dist:
+            closest_node = n
+            best_dist = current_dist
+
+
+@window.event
 def on_mouse_press(x, y, button, modifiers):
     if not edit:
         return
 
     if button == pgl.window.mouse.LEFT:
+        # Create Node
         node_list.append([x - cam_pos[0], y - cam_pos[1]])
     else:
-        best_node = 0
-        best_dist = 10000
-        for n in range(0, len(node_list)):
-            current_dist = sqrt((node_list[n][0] - (x - cam_pos[0]))**2 + (node_list[n][1] - (y - cam_pos[1]))**2)
-            if current_dist < best_dist:
-                best_node = n
-                best_dist = current_dist
-        
-        
         global selected_node
-        if best_node == selected_node:
+        if closest_node == selected_node: # if node is the same as selected
             return
-        if selected_node == -1:
-            selected_node = best_node
-        else:
-            edge_list.append([selected_node, best_node])
+        if selected_node == -1: # if there is no selected node
+            # Select node
+            selected_node = closest_node
+        else: # If twe different nodes have been selected
+            # Create edge
+            edge_list.append([selected_node, closest_node])
             selected_node = -1
 
 
@@ -147,5 +223,5 @@ def on_key_release(symbol, modifiers):
     if symbol == key.A or symbol == key.D:
         cam_input[0] = 0
 
-pgl.clock.schedule_interval(update, 1/30)
+pgl.clock.schedule_interval(update, 1/30) # 30 fps clock
 pgl.app.run()
